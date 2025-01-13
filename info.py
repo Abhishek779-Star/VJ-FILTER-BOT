8fb38b558c072f114f4e4425e4059201430f4874
Abhishek779-Star <abhishekbro779@gmail.com>
2025-01-13 21:31:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -107,7 +107,7 @@ REFERAL_PREMEIUM_TIME = environ.get('REFERAL_PREMEIUM_TIME', '1month') # time in
 PAYMENT_QR = environ.get('PAYMENT_QR', 'https://graph.org/file/47fed40f71a411a00e656-ac50cff8e8e973067d.jpg') # payment code picture url.
 
 PAYMENT_TEXT = environ.get('PAYMENT_TEXT', '<b><blockquote>💰💳𝐇𝐞𝐲 𝐏𝐫𝐞𝐦𝐢𝐮𝐦 𝐏𝐥𝐚𝐧𝐬 💲</blockquote>\n\n<blockquote>𝗣𝗿𝗲𝗺𝗶𝘂𝗺 𝗙𝗲𝗮𝘁𝘂𝗿𝗲</blockquote>\n\n<blockquote> ⭑⚝ 𝗪𝗶𝘁𝗵𝗼𝘂𝘁 𝗮𝗱𝘀\n    ๋࣭ ⭑⚝ 𝗡𝗼 𝘃𝗲𝗿𝗶𝗳𝗶𝗰𝗮𝘁𝗶𝗼𝗻\n    ๋࣭ ⭑⚝ 𝗗𝗶𝗿𝗲𝗰𝘁 𝗱𝗼𝘄𝗻𝗹𝗼𝗮𝗱 𝘁𝗼 𝗰𝗵𝗿𝗼𝗺𝗲\n    ๋࣭ ⭑⚝ 𝗗𝗶𝗿𝗲𝗰𝘁 𝗽𝗹𝗮𝘆 𝗶𝗻 𝘃𝗹𝗰/𝗺𝘅-𝗽𝗹𝗮𝘆𝗲𝗿 𝗮𝗻𝗱 𝗺𝗼𝗿𝗲 𝗽𝗹𝗮𝘆𝗲𝗿 𝗮𝘃𝗮𝗶𝗹𝗮𝗯𝗹𝗲\n    ๋࣭ ⭑⚝ 𝗙𝘂𝗹𝗹 𝗔𝗱𝗺𝗶𝗻 𝘀𝘂𝗽𝗽𝗼𝗿𝘁\n    ๋࣭ ⭑⚝ 𝗛𝗶𝗴𝗵 𝘀𝗽𝗲𝗲𝗱 𝗗𝗼𝘄𝗻𝗹𝗼𝗮𝗱\n    ๋࣭ ⭑⚝ 𝗧𝗩 𝗦𝗲𝗿𝗶𝗮𝗹𝘀\n    ๋࣭ ⭑⚝ 𝗡𝗲𝘄/𝗢𝗹𝗱 𝗠𝗼𝘃𝗶𝗲𝘀 𝗮𝗻𝗱 𝗦𝗲𝗿𝗶𝗲𝘀\n    ๋࣭ ⭑⚝ 𝗥𝗲𝗾𝘂𝗲𝘀𝘁 𝘄𝗶𝗹𝗹 𝗯𝗲 𝗰𝗼𝗺𝗽𝗹𝗲𝘁𝗲𝗱 𝗶𝗻 𝟭 𝗵𝗼𝘂𝗿 𝗶𝗳 𝗮𝘃𝗮𝗶𝗹𝗮𝗯𝗹𝗲｡</blockquote>\n\n<blockquote>   𝗔𝗹𝗹 𝗣𝗿𝗶𝗰𝗲 𝗟𝗶𝘀𝘁</blockquote>\n\n<blockquote>╭━━━━━━━━╮\n   • ₹10 - 1 Week/n   • ₹30 - 1 Month\n   • ₹60 - 2 Months\n   • ₹90 - 3 Months
- \n • ₹120 - 7 months\n╰━━━━━━━━╯</blockquote>\n\n<blockquote>ᴄᴏᴘʏᴛʜɪsᴜᴘɪɪᴅ</blockquote>\nᴜᴘɪɪᴅ➢<code>abhishek.kumar6395@fam</code>\n\n<blockquote>⚠️𝗦𝗲𝗻𝗱 𝗦𝗦 𝗔𝗳𝘁𝗲𝗿 𝗣𝗮𝘆𝗺𝗲𝗻𝘁⚠️ 𝗔𝗳𝘁𝗲𝗿 𝘀𝗲𝗻𝗱𝗶𝗻𝗴 𝗮 𝗦𝗰𝗿𝗲𝗲𝗻𝘀𝗵𝗼𝘁 𝗽𝗹𝗲𝗮𝘀𝗲 𝗴𝗶𝘃𝗲 𝘂𝘀 𝘀𝗼𝗺𝗲 𝘁𝗶𝗺𝗲 𝘁𝗼 𝗮𝗱𝗱 𝘆𝗼𝘂 𝗶𝗻 𝘁𝗵𝗲 𝗽𝗿𝗲𝗺𝗶𝘂𝗺 𝘃𝗲𝗿𝘀𝗶𝗼𝗻｡｡</blockquote></b>')
+ \n   • ₹120 - 7 months\n╰━━━━━━━━╯</blockquote>\n\n<blockquote>   ᴄᴏᴘʏ ᴛʜɪs ᴜᴘɪ ɪᴅ</blockquote>\n   ᴜᴘɪ ɪᴅ ➢ <code>abhishek.kumar6395@fam</code>\n\n<blockquote>⚠️𝗦𝗲𝗻𝗱 𝗦𝗦 𝗔𝗳𝘁𝗲𝗿 𝗣𝗮𝘆𝗺𝗲𝗻𝘁⚠️ 𝗔𝗳𝘁𝗲𝗿 𝘀𝗲𝗻𝗱𝗶𝗻𝗴 𝗮 𝗦𝗰𝗿𝗲𝗲𝗻𝘀𝗵𝗼𝘁 𝗽𝗹𝗲𝗮𝘀𝗲 𝗴𝗶𝘃𝗲 𝘂𝘀 𝘀𝗼𝗺𝗲 𝘁𝗶𝗺𝗲 𝘁𝗼 𝗮𝗱𝗱 𝘆𝗼𝘂 𝗶𝗻 𝘁𝗵𝗲 𝗽𝗿𝗲𝗺𝗶𝘂𝗺 𝘃𝗲𝗿𝘀𝗶𝗼𝗻｡｡</blockquote></b>')
 
 # Clone Information : If Clone Mode Is True Then Bot Clone Other Bots.
 
