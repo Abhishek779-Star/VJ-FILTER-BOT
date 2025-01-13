e779e8f6bbaf8900f3698d34642b9fd43a814c0d
Abhishek779-Star <abhishekbro779@gmail.com>
2025-01-13 23:48:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -117,7 +117,7 @@ CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "") # Necessary If clone 
 PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', 'MZAUTOFILTER') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/Movies_World_Request_Group_hdx')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/Movies_World_Support_Group')
 
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/MOVIES_WORLDZS')
 
