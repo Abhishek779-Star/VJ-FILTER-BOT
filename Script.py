bc286c458ed7a3eb3d77d2b5f7ca879e89e8af16
Abhishek779-Star <abhishekbro779@gmail.com>
2025-01-15 16:48:17 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -246,7 +246,8 @@ Nᴀᴍᴇ - {}"""
 
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
-    CAPTION = """<b>📂Fɪʟᴇɴᴀᴍᴇ  :<b> {file_name}</b>"""  <b> 📢 Jᴏɪɴ : @MOVIES_WORLDZS ❤️‍🔥</b>""" 
+    CAPTION = """<b>📂Fɪʟᴇɴᴀᴍᴇ  :{file_name} 
+    📢 Jᴏɪɴ : @MOVIES_WORLDZS ❤️‍🔥
 
 <b> Size ⚙️: {file_size}</b>""" 
 
