841b3a63be6030b727f75178ebc7aadbae1ee15e
Abhishek779-Star <abhishekbro779@gmail.com>
2025-03-05 10:01:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -121,7 +121,7 @@ GRP_LNK = environ.get('GRP_LNK', 'https://t.me/Movies_World_Request_Group_hdx')
 
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/MOVIES_WORLDZS')
 
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Movies_World_Support_Group') # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '+4LbUxZR-F6NmODQ1') # Support Chat Link Without https:// or @
 
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/AV_King1')
 
